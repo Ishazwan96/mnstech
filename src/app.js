650e3b05306d38559abb28a68aa0eb14e5e56bd2
ishazwan <ishazwan96@gmail.com>
2025-03-10 21:29:16 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ document.addEventListener('alpine:init', () => {
             { id:9,name:'16 Channel', img:'1.png', price: 7880,
                 features: ['4K Resolution','1 x DVR Recorder','1 x 6TB HDD','16 x Camera (Dome/Bullet)','Wiring/Installation']
              },
-            { id:10,name:'Wiring/Installation', img:'db.png', price: 0,
+            { id:10,name:'Wiring/Installation', img:'1.png', price: 0,
                 features: ['Wiring Soket','Wiring Lampu','Wiring Kipas','Wiring DB','lain-lain']
              
         ],
