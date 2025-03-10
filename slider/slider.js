0082c8bd36be1aaf9c40dd6ce48a4ec396691800
ishazwan <ishazwan96@gmail.com>
2025-03-10 23:28:13 +0800

    Update slider.js

--- slider/slider.js
+++ slider/slider.js
@@ -53,3 +53,61 @@ const slider = document.querySelector('.slider');
 slider.addEventListener('click', (e) => {
   e.stopPropagation(); // Stop event propagation to prevent closing the shopping cart
 });
+
+
+
+let list = document.querySelector('.project .list');
+let items = document.querySelectorAll('.project .list .item');
+let dots = document.querySelectorAll('.project .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function() {
+
+    if (active + 1 > lengthItems) {
+
+        active = 0;
+    } else {
+        active = active + 1;
+    }
+    reloadSlider();
+}
+
+prev.onclick = function(){
+
+    if(active - 1 < 0) { 
+        active = lengthItems;
+    }else{
+        active = active - 1;
+    }
+    reloadSlider();
+}
+let refreshSlider = setInterval(()=> {next.click()}, 3000);
+function reloadSlider() {
+
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(()=> {next.click()}, 3000);
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click',function() {
+        active = key;
+        reloadSlider();
+    })
+})
+
+// Prevent shopping cart from closing when slider is clicked
+const slider = document.querySelector('.project');
+
+slider.addEventListener('click', (e) => {
+  e.stopPropagation(); // Stop event propagation to prevent closing the shopping cart
+});
