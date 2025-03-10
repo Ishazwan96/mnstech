81107b2f6e5def8e490c5c16bb994ec3e1d088ee
ishazwan <ishazwan96@gmail.com>
2025-03-10 19:49:01 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
         items: [
-            { id:1,name:'4 Channel', img:'gambar2.jpg', price: 1680,
+            { id:1,name:'4 Channel', img:'1.png', price: 1680,
                 features: ['FULL HD','1 x DVR Recorder','1 x 1TB HDD','4 x Camera (Dome/Bullet)','Wiring/Installation']
             },
             { id:2,name:'8 Channel', products:'1.png', price: 3080,
